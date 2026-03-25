5ca973a48b51402e1614c42b12c3e923c826daeb
polovni <luka.polovic11@gmail.com>
2026-03-25 20:23:00 +0100

    Adding commons cli library and mvn to project Simple CLI parsing logic

--- Main.java
+++ Main.java
@@ -1,5 +1,24 @@
+import org.apache.commons.cli.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Options options = new Options();
+
+        options.addOption("h", "help", false, "show help");
+        options.addOption("t", "type", false, "Type selected");
+
+        CommandLineParser parse = new DefaultParser();
+        try {
+            CommandLine cmd = parse.parse(options, args);
+            if(cmd.hasOption("h")) {
+                System.out.println("I AM HELPING YOU");
+            } else if(cmd.hasOption("type")) {
+                System.out.println("You selected type");
+            } else {
+                System.out.println("Either use -h or -t flags");
+            }
+        } catch (ParseException e) {
+            System.out.println(e);
+        }
     }
 }
