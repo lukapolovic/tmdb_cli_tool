57ede1dda4af410d7721fb3e4e04c96ad97a8f0b
polovni <luka.polovic11@gmail.com>
2026-03-30 17:27:06 +0200

    Creating Movie class

---
+++ Movie.java
@@ -0,0 +1,104 @@
+import java.util.Date;
+
+public class Movie {
+    long id;
+    boolean isAdultRated;
+    String originalLanguage;
+    String title;
+    String overview;
+    double popularity;
+    Date releaseDate;
+    int rating;
+    int ratingVotes;
+
+    public Movie(int ratingVotes, int rating, Date releaseDate,
+                 double popularity, String overview, String title,
+                 String originalLanguage, boolean isAdultRated, long id) {
+        this.ratingVotes = ratingVotes;
+        this.rating = rating;
+        this.releaseDate = releaseDate;
+        this.popularity = popularity;
+        this.overview = overview;
+        this.title = title;
+        this.originalLanguage = originalLanguage;
+        this.isAdultRated = isAdultRated;
+        this.id = id;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public boolean isAdultRated() {
+        return isAdultRated;
+    }
+
+    public void setAdultRated(boolean adultRated) {
+        isAdultRated = adultRated;
+    }
+
+    public String getOriginalLanguage() {
+        return originalLanguage;
+    }
+
+    public void setOriginalLanguage(String originalLanguage) {
+        this.originalLanguage = originalLanguage;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public double getPopularity() {
+        return popularity;
+    }
+
+    public void setPopularity(double popularity) {
+        this.popularity = popularity;
+    }
+
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(Date releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public int getRatingVotes() {
+        return ratingVotes;
+    }
+
+    public void setRatingVotes(int ratingVotes) {
+        this.ratingVotes = ratingVotes;
+    }
+
+    @Override
+    public String toString() {
+        return "Movie{}";
+    }
+}
