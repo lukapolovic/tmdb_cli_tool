8fde8dce660ee279ea90945deac32a34a67866d9
polovni <luka.polovic11@gmail.com>
2026-04-01 18:38:40 +0200

    Creating initial CLI GUI structure

--- MyWindow.java
+++ MyWindow.java
@@ -10,24 +10,39 @@ public class MyWindow extends BasicWindow {
 
         ActionListBox menu = new ActionListBox();
 
-        menu.addItem("Top Panel", () -> {
-            MessageDialog.showMessageDialog(gui, "Top", "Top pannel selected");
+        menu.addItem("Now Playing Movies", () -> {
+            MessageDialog.showMessageDialog(
+                    gui,
+                    "Now Playing Movies",
+                    "Here are the top 10 movies that are being screened right now!"
+            );
         });
 
-        menu.addItem("Middle Panel", () -> {
-            BasicWindow newWindow = new BasicWindow("Middle panel");
-
-            Panel p = new Panel();
-            p.addComponent(new Label("Middle panel content"));
-            p.addComponent(new Button("Back", newWindow::close));
-
-            newWindow.setComponent(p);
-            gui.addWindowAndWait(newWindow);
+        menu.addItem("Popular Movies", () -> {
+            MessageDialog.showMessageDialog(
+                    gui,
+                    "Popular Movies",
+                    "Here are the top 10 most popular movies!"
+            );
         });
 
-        menu.addItem("Bottom Panel", () -> {
-            MessageDialog.showMessageDialog(gui, "Bottom", "Bottom pannel selected");
+        menu.addItem("Top Rated Movies", () -> {
+            MessageDialog.showMessageDialog(
+                    gui,
+                    "Top Rated Movies",
+                    "Here are the top 10 best rated movies of all time!"
+            );
         });
+
+        menu.addItem("Upcoming Movies", () -> {
+            MessageDialog.showMessageDialog(
+                    gui,
+                    "Upcoming Movies",
+                    "Here are the top 10 most popular upcoming movies!"
+            );
+        });
+
+        menu.addItem("Exit", MyWindow.this::close);
 
         panel.addComponent(menu);
         setComponent(panel);
